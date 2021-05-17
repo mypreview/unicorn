7336f65efff9cc96e8cf334a6a2d1fc7739a51f4
Sajedeh Gooklani <gookalani@mail.com>
2021-05-17 23:41:53 +0300

    Added reply icon

--- react/index.js
+++ react/index.js
@@ -1,5 +1,6 @@
 export { default as helicopter } from './library/helicopter';
 export { default as link } from './library/link';
 export { default as pencil } from './library/pencil';
+export { default as pencil } from './library/reply';
 export { default as search } from './library/search';
 export { default as unicorn } from './library/unicorn';
