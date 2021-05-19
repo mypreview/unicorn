15321ad680c2692ce03c8ebd379053ac6c618df1
Sajedeh Gooklani <gookalani@mail.com>
2021-05-19 20:20:18 +0300

    Added check circle icon

--- react/index.js
+++ react/index.js
@@ -1,3 +1,4 @@
+export { default as checkCircle } from './library/check-circle';
 export { default as helicopter } from './library/helicopter';
 export { default as link } from './library/link';
 export { default as pencil } from './library/pencil';
